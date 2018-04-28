a1608d14d507089ce2185a2c0527c5a542372e92
Aaron Bao <aaron.bao@cf.manpower.com.cn>
2018-04-28 09:49:19 +0800

    updata backend get token

--- server/config.js
+++ server/config.js
@@ -5,5 +5,5 @@ module.exports = {
     "wx_appid": "wx705a4d4af2a47e55",
     "wx_secret": "cc98d52f5449abd0be2fef8145261966",
     "wx_token":"ActivityDemoToken",
-    "wx_cb_domain": "bb4c14bc.ngrok.io"
+    "wx_cb_domain": "6a293b6d.ngrok.io"
 }
