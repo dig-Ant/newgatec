f925efb81b4d93876302dbafc2ae34014bc66096
ao.lv <ao.lv@cf.manpower.com.cn>
2018-05-07 18:41:40 +0800

    添加验证码验证的测试模块，修改checkCode返回值的问题

--- test/modeltest/smsSend.js
+++ test/modeltest/smsSend.js
@@ -21,35 +21,27 @@ describe('sms', function() {
   });
 
   describe('smsCode',() =>{
-    it('should return success',(done)=>{
-      superagent.post(serverUrl+'Sms/smsCode')
-      .send({
-        "phone":"18651833910",
-        "type":1,
-        "sign":"675",
-        "modelId":"3839",
-        "content":"{code}",
-        "codeType":1
-      })
+    // it('should return success',(done)=>{
+    //   superagent.post(serverUrl+'Anons/codeSms')
+    //   .send({
+    //     "phone":"18651833910",
+    //     "type":1
+    //   })
 
-      .end((err,res)=>{
-        expect(err).toEqual(null);
-        expect(res.status).toEqual(200);
-        expect(res.body.result.code).toEqual(1);
-        expect(typeof res.body.result.data.smUuid).toEqual(string);
-        done();
-      })
-    })
+    //   .end((err,res)=>{
+    //     expect(err).toEqual(null);
+    //     expect(res.status).toEqual(200);
+    //     expect(res.body.result.code).toEqual(1);
+    //     expect(typeof res.body.result.data.smUuid == 'string').toEqual(true);
+    //     done();
+    //   })
+    // })
 
     it('should return pnone number error',()=>{
-      superagent.post(serverUrl+'Sms/smsCode')
+      superagent.post(serverUrl+'Anons/codeSms')
       .send({
         "phone":"1865183391",
-        "type":1,
-        "sign":"675",
-        "modelId":"3839",
-        "content":"{code}",
-        "codeType":1
+        "type":1
       })
 
       .end((err,res)=>{
@@ -61,26 +53,22 @@ describe('sms', function() {
       })
     })
 
-    it('should return pnone number error',()=>{
-      superagent.post(serverUrl+'Sms/smsCode')
+    it('should return formed error',()=>{
+      superagent.post(serverUrl+'Anons/codeSms')
       .send({
         "phone":"1865183391",
-        "type":1,
-        "sign":"675",
-        "modelId":"3839",
-        "content":"{code}",
-        "codeType":1
+        
       })
 
       .end((err,res)=>{
         expect(err).toEqual(null);
         expect(res.status).toEqual(200);
         expect(res.body.result.code).toEqual(0);
-        expect(res.body.result.msg).toEqual('手机号码格式不正确');
+        expect(res.body.result.msg).toEqual('type不能为空');
         done();
       })
     })
-  
+    
   
   })
 
