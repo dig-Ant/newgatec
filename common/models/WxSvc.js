c16f2dc3cbfdb36766510a3d3efe58e61e7b84c6
Ze Huang Yang <Ze.Yang@cf.manpower.com.cn>
2018-05-11 18:15:32 +0800

    后台添加 getwxconfig 接口

--- common/models/WxSvc.js
+++ common/models/WxSvc.js
@@ -6,6 +6,9 @@ let fs = require('fs');
 let util = require('../util/util');
 let request = require('request');
 
+
+
+
 module.exports = function (WxSvc) {
 
   WxSvc.CheckCache = function () {
@@ -34,12 +37,14 @@ module.exports = function (WxSvc) {
     WxSvc.CheckCache();
     if (!WxSvc.Cache.api) {
       let api = new WechatAPI(config.wx_appid, config.wx_secret);
+      api.getTicket_ify = util.promisify(api.getTicket.bind(api));
+      api.getJsConfig_ify = util.promisify(api.getJsConfig.bind(api));
       WxSvc.Cache.api = api;
     }
     return WxSvc.Cache.api;
   }
- 
- 
+
+
   WxSvc.rebuildUrl = function (url, token) {
     let ourl = util.removeQSFromUrl(url, "auth_token");
     let conn = ourl.indexOf("?") > 0 ? "&" : "?";
@@ -48,9 +53,26 @@ module.exports = function (WxSvc) {
 
   WxSvc.defCB = (err, result) => { console.log(err) }
 
+  WxSvc.getWxConfig = async function (paramObj) {
+    console.log('paramObj---',paramObj);
 
-  WxSvc.getAccessToken = async function (code) { 
-    console.log('code---',code);
+    let wx_api = WxSvc.getApi(); 
+
+    let ticket_ify = await wx_api.getTicket_ify();
+    console.log('ticket_ify---', ticket_ify); 
+    var param = {
+      debug: paramObj.debug,
+      jsApiList: paramObj.jsApiList,
+      url: paramObj.url
+    };
+    let getJsConfig_ify = await wx_api.getJsConfig_ify(param);
+    console.log('getJsConfig_ify-',getJsConfig_ify);
+    return getJsConfig_ify;
+  }
+
+  WxSvc.getAccessToken = async function (code) {
+  
+    console.log('code---', code);
     let client = WxSvc.getClient();
     // 请求微信 access_token 
     let wx_accessToken = await client.getAccessToken_ify(code.code);
@@ -58,50 +80,60 @@ module.exports = function (WxSvc) {
     let wx_userinfo = await client.getUser_ify(wx_accessToken.data.openid);
     //通过请求头返回token
     let requestRes = util.promisify(request.bind(this));
-    let wx_tokenResult =await requestRes({ 
+    let wx_tokenResult = await requestRes({
       method: "POST",
       url: "http://10.186.1.222:8888/api/get_access_token",
+      // url: "http://172.16.1.139:8888/userhost/api/inner/get_access_token",
       form: {
         openid: wx_accessToken.data.openid,
-        api_name:"wx_svc",
+        api_name: "wx_svc",
       }
-    }); 
-    let cf_tokenResult =await requestRes({ 
+    });
+    let cf_tokenResult = await requestRes({
       method: "POST",
       url: "http://10.186.1.222:8888/api/get_access_token",
+      // url: "http://172.16.1.139:8888/userhost/api/inner/get_access_token",
       form: {
         openid: wx_accessToken.data.openid,
-        api_name:"cf_api",
+        api_name: "cf_api",
       }
-    }); 
+    });
     let token_wx = util.toJson(wx_tokenResult.body).body.access_token;
     let token_cf = util.toJson(cf_tokenResult.body).body.access_token;
     // let api = WxSvc.getApi();
     // cb(null,token)
-    console.log('token--',wx_tokenResult.body);
+    console.log('token--', wx_tokenResult.body);
     return {
       token_wx,
       token_cf
     };
   }
-  WxSvc.getInfo = async function (code) { 
+  WxSvc.getInfo = async function (code) {
 
-    console.log('res---',code);
+    console.log('res---', code);
 
     return code;
   }
 
   WxSvc.remoteMethod('getAccessToken', {
     http: { verb: 'post' },
-    accepts: [{ arg: 'code', type: 'object',http: { source: 'body' } }],
+    accepts: [{ arg: 'code', type: 'object', http: { source: 'body' } }],
     returns: {
       arg: 'res', type: 'object'
     }
   })
 
+  WxSvc.remoteMethod('getWxConfig', {
+    http: { verb: 'post' },
+    accepts: [{ arg: 'param', type: 'object', http: { source: 'body' } }],
+    returns: {
+      arg: 'body', type: 'object'
+    }
+  })
+  
   WxSvc.remoteMethod('getInfo', {
     http: { verb: 'post' },
-    accepts: [{ arg: 'code', type: 'object',http: { source: 'body' } }],
+    accepts: [{ arg: 'code', type: 'object', http: { source: 'body' } }],
     returns: {
       arg: 'res', type: 'object'
     }
