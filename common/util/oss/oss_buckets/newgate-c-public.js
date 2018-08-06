72d60b16d8ee4cbcc253b6ef3e57f3306a1042e5
ao.lv <ao.lv@cf.manpower.com.cn>
2018-08-06 19:09:31 +0800

    完成私密文件上传以及公用文件上传接口,删除文件接口,以及私密base64文件上传以及公用base64文件上传接口

---
+++ common/util/oss/oss_buckets/newgate-c-public.js
@@ -0,0 +1,9 @@
+let OSS = require('ali-oss');
+const newgate_c_tast = new OSS({
+  region: 'oss-cn-shanghai',// 华北2
+  accessKeyId: 'LTAILsBXWVclWJ9B',
+  accessKeySecret: 'amlucjVmtUp8XjvzOR4p1z5nO1cd1Y',
+  bucket: 'newgate-c-public',
+  secure:false
+});
+module.exports = newgate_c_tast;
