12fb90ad6897f9c5c4b79bb69a4fdb7545d6cee3
ao.lv <ao.lv@cf.manpower.com.cn>
2018-08-24 18:14:59 +0800

    完成薪酬发放所需字段的添加，以及发送渠道的关键字的设计，以及发送所需专用默认字段的添加,以及完成所有表的字段设计以及数据模型生成

--- server/models/mc_model.js
+++ server/models/mc_model.js
@@ -3,32 +3,28 @@ module.exports = function(MC_Model) {
   MC_Model.method_creat_mc = (obj)=>{
     return new Promise(async (resolve,reject)=>{
 
-
-      switch (obj.mc_method) {
-      case 0:
-        resolve('ok');
-        let _sms = await MC_Model.app.models.MC.sms_creat_mc();
-        break;
-        
-      case 1:
-        resolve('ok');
-        let _wechat = await MC_Model.app.models.MC.wechat_creat_mc();
-        break;
-        
-      case 2:
-        resolve('ok');
-        let _email = await MC_Model.app.models.MC.email_creat_mc();
-        break;
-
-      case 3:
-        
-        resolve('ok'); 
-        break;
-
-      default:
-        reject(new Error('类型错误'));
-        break;
+      try {
+        let result = {};
+        if (obj.mc_method.sms == true) {
+          let _sms = await MC_Model.app.models.MC.sms_creat_mc(obj);
+          console.log('sms',_sms.body);
+          result._sms = _sms.body;
+        }
+        if (obj.mc_method.wechaat == true) {
+          let _wechat = await MC_Model.app.models.MC.wechat_creat_mc(obj);
+          console.log('_wechat',_wechat);
+          result._wechat = _wechat.body;
+        }
+        if (obj.mc_method.email == true) {
+          let _email = await MC_Model.app.models.MC.email_creat_mc(obj);
+          console.log('_email',_email);
+          result._email = _email.body;
+        }
+        resolve(result);
+      } catch (error) {
+        reject(error);
       }
+
     });
   };
 };
